ee2c5599bc143928d9bfcd7ba80c2dffbd187bcd
Ravi Kumar <ravikumar956078@gmail.com>
2023-04-17 13:37:49 +0530

    Added toggle like api

--- src/index.js
+++ src/index.js
@@ -19,11 +19,11 @@ app.listen(3000, async () => {
   await connect();
   console.log("Mongo db connected");
 
-  const userRepo = new UserRepository();
-  const tweetRepo = new TweetRepository();
-  const tweets = await tweetRepo.getAll(0, 10);
-  const users = await userRepo.getAll();
+  // const userRepo = new UserRepository();
+  // const tweetRepo = new TweetRepository();
+  // const tweets = await tweetRepo.getAll(0, 10);
+  // const users = await userRepo.getAll();
 
-  const likeService = new LikeService();
-  await likeService.toggleLike(tweets[0].id, "Tweet", users[0].id);
+  // const likeService = new LikeService();
+  // await likeService.toggleLike(tweets[0].id, "Tweet", users[0].id);
 });
